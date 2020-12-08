cc4057c4e4d5a37d46df099629a0cad0088f801b
Randy Parker <randallbparker@gmail.com>
2020-12-08 17:19:11 -0600

    AddJobViewModel and AddJob form semi functional

---
+++ TechJobsPersistent/ViewModels/AddJobViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechJobsPersistent.Models;
+
+namespace TechJobsPersistent.ViewModels
+{
+    public class AddJobViewModel
+    {
+        public string Name { get; set; }
+        public int EmployerId { get; set; }
+        public List<SelectListItem> Employers { get; set; }
+        public AddJobViewModel(List<Employer> employers)
+        {
+            Employers = new List<SelectListItem>();
+
+            foreach (var employee in employers)
+            {
+                Employers.Add(new SelectListItem
+                {
+                    Value = employee.Id.ToString(),
+                    Text = employee.Name
+                });
+            }
+        }
+    }
+}
