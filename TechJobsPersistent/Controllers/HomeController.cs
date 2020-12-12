e957eaa5f7e5f87c58d43b5b96e8862e11398efb
Randy Parker <randallbparker@gmail.com>
2020-12-12 08:05:13 -0600

    Working toward adding skills to AddJobViewModel

--- TechJobsPersistent/Controllers/HomeController.cs
+++ TechJobsPersistent/Controllers/HomeController.cs
@@ -32,20 +32,40 @@ namespace TechJobsPersistent.Controllers
         [HttpGet("/Add")]
         public IActionResult AddJob()
         {
-            List<Employer> employers = context.Employers.ToList();
-
-            AddJobViewModel addJobViewModel = new AddJobViewModel(employers);
+            AddJobViewModel addJobViewModel = new AddJobViewModel(context.Employers.ToList(), context.Skills.ToList());
             return View(addJobViewModel);
         }
 
+        [HttpPost]
         public IActionResult ProcessAddJobForm(AddJobViewModel addJobViewModel)
         {
             if (ModelState.IsValid)
             {
                 // save and redirect
+                // In the ProcessAddJobForm() method, pass in a new parameter: an array of strings called selectedSkills. When we allow the user to select multiple checkboxes, the user’s selections are stored in a string array. The way we connect the checkboxes together is by giving the name attribute on the<input> tag the name of the array.In this case, each<input> tag on the form for the skills checkboxes should have "selectedSkills" as the name.
+                // After you add a new parameter, you want to set up a loop to go through each item in selectedSkills.This loop should go right after you create a new Job object and before you add that Job object to the database.
+                // Inside the loop, you will create a new JobSkill object with the newly - created Job object.You will also need to parse each item in selectedSkills as an integer to use for SkillId.
+                // Add each new JobSkill object to the DbContext object, but do not add an additional call to SaveChanges() inside the loop! One call at the end of the method is enough to get the updated info to the database.
             }
 
-            return View("Add", addJobViewModel);
+            // We need to repopulate the dropdown list if our ModelState is invalid
+            // ... this can't possibly be the best way to do this
+            if (addJobViewModel.Employers == null)
+            {
+                List<Employer> employers = context.Employers.ToList();
+                addJobViewModel.Employers = new List<SelectListItem>();
+                employers.ForEach(e => addJobViewModel.Employers.Add(new SelectListItem
+                {
+                    Value = e.Id.ToString(),
+                    Text = e.Name
+                }));
+            }
+            if (addJobViewModel.Skills == null)
+            {
+                addJobViewModel.Skills = context.Skills.ToList();
+            }
+
+            return View("AddJob", addJobViewModel);
         }
 
         public IActionResult Detail(int id)
--- TechJobsPersistent/ViewModels/AddJobViewModel.cs
+++ TechJobsPersistent/ViewModels/AddJobViewModel.cs
@@ -1,20 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using TechJobsPersistent.Data;
 using TechJobsPersistent.Models;
 
 namespace TechJobsPersistent.ViewModels
 {
     public class AddJobViewModel
     {
+        [Required(ErrorMessage ="Name is required")]
         public string Name { get; set; }
+        [Required(ErrorMessage ="Employer ID Missing!")]
         public int EmployerId { get; set; }
+
         public List<SelectListItem> Employers { get; set; }
-        public AddJobViewModel(List<Employer> employers)
+
+        public List<Skill> Skills { get; set; }
+
+        public AddJobViewModel(List<Employer> employers, List<Skill> skills)
         {
             Employers = new List<SelectListItem>();
+            Skills = new List<Skill>();
+
+            Skills = skills.ToList(); // shallow or okay?
 
             foreach (var employee in employers)
             {
@@ -22,9 +33,10 @@ namespace TechJobsPersistent.ViewModels
                 {
                     Value = employee.Id.ToString(),
                     Text = employee.Name
-                });
+                }); ;
             }
         }
-        public AddJobViewModel() { }
+
+        public AddJobViewModel(){}
     }
 }
